03aef56cb87747d4823ac4481615bee7e7b84cd3
olelit <litasov00@gmail.com>
2021-01-09 01:11:07 +0300

    Добавьте файлы проекта.

---
+++ SnakeWinForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using SnakeWinForm.App.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeWinForm
+{
+    public partial class Form1 : Form
+    {
+
+        MainApp _App;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _App = new MainApp();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
